6a464e46f30784d1111198074728caa3c8e9ddd9
Nazar Taran <nazar.taran.id@gmail.com>
2023-09-03 16:30:13 +0200

    Initial fixes

--- bot/types.py
+++ bot/types.py
@@ -3,5 +3,5 @@ from bot.utils_lib import helper
 
 class Log(helper.HelperMode):
     mode = helper.HelperMode.SCREAMING_SNAKE_CASE
-    DELETION = 'DELETION'
-    USERNAME_CHANGE = 'USERNAME_CHANGE'
+    DELETION = helper.Item()
+    USERNAME_CHANGE = helper.Item()
