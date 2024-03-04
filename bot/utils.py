0c84aa54e11bad95d90b818cef9ccd21332546f2
Nazar Taran <nazar.taran.id@gmail.com>
2024-03-04 08:43:12 +0100

    Add ValueError handling

--- bot/utils.py
+++ bot/utils.py
@@ -17,6 +17,9 @@ async def loop_wrapper(func, sleep_time, *args, **kwargs):
         except FloodWaitError as e:
             logging.warning(f'Flood wait {e.seconds} seconds')
             await sleep(e.seconds)
+        except ValueError as e:
+            logging.error(e)
+            await sleep(60)
 
 
 async def collect_media_group(client: TelegramClient, post: types.Message):
