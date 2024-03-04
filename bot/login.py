ad036cbffc4ff62620c182c1ca552b883323e6c2
Nazar Taran <nazar.taran.id@gmail.com>
2024-03-04 08:42:59 +0100

    Add receive_updates=False

--- bot/login.py
+++ bot/login.py
@@ -9,8 +9,8 @@ from app.settings import BASE_DIR, API_ID, API_HASH, HOST_FUNC_COUNT
 def main(number, host=False):
     if host:
         for i in range(HOST_FUNC_COUNT):
-            client = TelegramClient(f'{BASE_DIR}/sessions/host{i}', API_ID, API_HASH)
+            client = TelegramClient(f'{BASE_DIR}/sessions/host{i}', API_ID, API_HASH, receive_updates=False)
             client.start(lambda: number)
     else:
-        client = TelegramClient(f'{BASE_DIR}/sessions/{number}', API_ID, API_HASH)
+        client = TelegramClient(f'{BASE_DIR}/sessions/{number}', API_ID, API_HASH, receive_updates=False)
         client.start(lambda: number)
